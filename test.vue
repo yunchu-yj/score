b4c8881eea97147c81749015ed35b7841db4fc1c
彭家皓 <16033164+peng-jiahao2024@user.noreply.gitee.com>
2026-04-27 00:24:32 +0800

    feat: 添加基础计数器组件

---
+++ test.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+<template>
+    <div>
+        <h1 class="title" style="text-align: center;" @click="count++">当前计数: {{ count }}</h1>
+    </div>
+</template> 
